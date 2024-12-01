af96aa23919f1f18279690af6f4f4a8fb4712c66
gman <gman>
2024-12-01 21:44:42 +0300

    first and second chapters ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,29 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    redirects: {
+        '/': '/chapters',
+    },
+    integrations: [starlight({
+        title: 'Веб-картография',
+        locales: {
+            root: {
+                label: 'Русский',
+                lang: 'ru',
+            }
+        },
+        social: {
+            github: 'https://github.com/withastro/starlight',
+        },
+        sidebar: [
+            {
+                label: 'Введение в веб-картографию',
+                autogenerate: { directory: 'chapters' },
+            }
+        ],
+		}), react()],
 });
